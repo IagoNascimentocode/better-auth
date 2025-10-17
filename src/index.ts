3f046c3f2a7652f798cf7fd894ec5b673d4f2960
iago.nascimento <iagonascimento@2bebot.com.br>
2025-10-17 02:17:51 -0300

    feat: email and password

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,56 @@
+import { openapi } from "@elysiajs/openapi";
 import { Elysia } from "elysia";
+import { auth } from "@/auth";
+import { z } from "zod";
+import { betterAuthPlugin, OpenAPI } from "./http/plugins/better-auth";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+      cors({
+          origin: 'http://localhost:5173',
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+          credentials: true,
+          allowedHeaders: ['Content-Type', 'Authorization']
+      })
+  )
+  .use(
+    openapi({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    }),
+  )
+  .use(betterAuthPlugin)
+  .get("/", () => "Hello Elysia")
+  .get(
+    "/users/:id",
+    ({ params, session, user }) => {
+
+
+      return { session };
+    },
+    {
+      auth: true,
+      detail: {
+        summary: "Get user by id",
+        description: "Get user by id",
+      },
+      tags: ["User"],
+      params: z.object({
+        id: z.string(),
+      }),
+      response: {
+        200: z.object({
+          id: z.string(),
+          name: z.string(),
+        }),
+      },
+    },
+  )
+  .listen(3333);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
